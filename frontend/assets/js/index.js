1562172e42b7884c446945fd1d4379b74571e24f
Putra Christian <christianp@garena.co.id>
2021-06-02 15:55:30 +0700

    initial

---
+++ frontend/assets/js/index.js
@@ -0,0 +1,97 @@
+$(function () {
+	const io = window.io
+	const socket = io.connect("ws://10.23.114.69:3000/", {
+		transports: ["websocket"],
+	})
+	socket.on("connect", () => {
+		console.log("Connected")
+	})
+
+	socket.on("disconnect", (reason) => {
+		console.log(reason)
+	})
+
+	$(".app").load("nickname.html", function () {
+		$(".btn-submit").click(function (e) {
+			e.preventDefault()
+			localStorage.setItem("nickname", $("#nickname").val())
+
+			$(".app").load("room.html", function () {
+				$(".create-room").click(function () {
+					socket.emit("create_room", { nickname: "home" })
+					window.localStorage.setItem("status", "home")
+					toMatch()
+				})
+
+				$(".btn-join").click(function () {
+					socket.emit("join_room", {
+						room_number: $("#room_id").val(),
+						nickname: "visitor",
+					})
+					window.localStorage.setItem("status", "visitor")
+					toMatch()
+				})
+			})
+		})
+	})
+
+	// MATCH
+	const toMatch = () =>
+		$(".app").load("match.html", () => {
+			let option = `
+                <span class='choice-title'>Choose wisely</span>
+                <div class='choices'>
+                    <div class='choice' id='r' data-choice="stone">
+                        <img src='https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/stone.png' alt="it's a rock" />
+                    </div>
+                    <div class='choice' id='p' data-choice="paper">
+                        <img src='https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/paper.png' alt="it's a paper" />
+                    </div>
+                    <div class='choice' id='s' data-choice="scissors">
+                        <img
+                            src='https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/scissors.png'
+                            alt="it's a scissors"
+                        />
+                    </div>
+                </div>
+            `
+
+			socket.on("emit_room", (e) => {
+				if (
+					window.localStorage.getItem("status") === "home" &&
+					!e?.home?.state
+				) {
+					$(".player-1 .bottom").html(option)
+				} else {
+					$(".player-1 .bottom").html("<span class='text'>Please Wait</span>")
+				}
+
+				if (
+					window.localStorage.getItem("status") === "visitor" &&
+					!e?.visitor?.state
+				) {
+					$(".player-2 .bottom").html(option)
+				} else {
+					$(".player-2 .bottom").html("<span class='text'>Please Wait</span>")
+				}
+
+				$(".roomid").html(e.room_number)
+				$(".score span:first").html(e?.home?.point)
+				$(".score span:last").html(e?.visitor?.point)
+				$(".player-1 .chosen").html(
+					`<img alt='' src='https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/${
+						e?.winner && e?.home?.state
+					}.png'} />`
+				)
+				$(".player-2 .chosen").html(
+					`<img alt='' src='https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/${
+						e?.winner && e?.visitor?.state
+					}.png'} />`
+				)
+				$(".player .choice").click(function (e) {
+					let val = $(this).data("choice")
+					socket.emit("play", { chosen: val })
+				})
+			})
+		})
+})
---
+++ socket/index.js
@@ -0,0 +1,171 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+const ROOM_NUMBER_STR = "room_number"
+const PLAYER_JOIN_ROOM = "emit_room"
+const PLAYER_LEAVE_ROOM = "emit_player_leave_room"
+const PLAYER_PLAY = "emit_room"
+
+const PLAYER_1_STR = "player_1"
+const PLAYER_2_STR = "player_2"
+const NICKNAME_STR = "nickname"
+
+let roomList = {}
+let roomNumberList = []
+let playerList = {}
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html")
+})
+
+io.on("connection", (socket) => {
+	socket.on("disconnect", () => {
+		// console.log(socket);
+	})
+
+	socket.on("create_room", (payload) => {
+		try {
+			let roomNumber = ""
+			while (true) {
+				roomNumber = ""
+				for (let i = 0; i < 4; i++) {
+					roomNumber += Math.floor(Math.random() * 10)
+				}
+				if (!roomNumberList.includes(roomNumber)) {
+					break
+				}
+			}
+			roomList[roomNumber] = {
+				room_number: roomNumber,
+				counter: 0,
+				[payload[NICKNAME_STR]]: {
+					// nickname: payload[NICKNAME_STR]
+					point: 0,
+				},
+				// player_2: {
+				//   nickname: null
+				// }
+			}
+			playerList[socket.id] = {
+				room_number: roomNumber,
+				player: 1,
+				nickname: payload[NICKNAME_STR],
+			}
+			socket.join(roomNumber)
+			io.to(roomNumber).emit(PLAYER_JOIN_ROOM, roomList[roomNumber])
+		} catch (ex) {
+			socket.emit(PLAYER_JOIN_ROOM, { msg: "Invalid Params" })
+		}
+	})
+
+	socket.on("leave_room", (payload) => {
+		if (roomList[payload[ROOM_NUMBER_STR]] == null) {
+			socket.emit(PLAYER_LEAVE_ROOM, false)
+		} else {
+			if (
+				roomList[payload[ROOM_NUMBER_STR]][PLAYER_1_STR] ==
+				payload[NICKNAME_STR]
+			) {
+				roomList[payload[ROOM_NUMBER_STR]][PLAYER_1_STR] = null
+			} else {
+				roomList[payload[ROOM_NUMBER_STR]][PLAYER_2_STR] = null
+			}
+			socket.leave(payload[ROOM_NUMBER_STR])
+			io.to(payload[ROOM_NUMBER_STR]).emit(
+				PLAYER_LEAVE_ROOM,
+				payload[NICKNAME_STR]
+			)
+		}
+	})
+
+	socket.on("join_room", (payload) => {
+		if (roomList[payload[ROOM_NUMBER_STR]] == null) {
+			socket.emit(PLAYER_JOIN_ROOM, { msg: "Invalid Room Number" })
+		} else {
+			console.log(roomList[payload[ROOM_NUMBER_STR]])
+			roomList[payload[ROOM_NUMBER_STR]][payload[NICKNAME_STR]] = {
+				point: 0,
+			}
+
+			playerList[socket.id] = {
+				room_number: payload[ROOM_NUMBER_STR],
+				player: 2,
+				nickname: payload[NICKNAME_STR],
+			}
+			socket.join(payload[ROOM_NUMBER_STR])
+			io.to(payload[ROOM_NUMBER_STR]).emit(
+				PLAYER_JOIN_ROOM,
+				roomList[payload[ROOM_NUMBER_STR]]
+			)
+		}
+	})
+
+	socket.on("play", (payload) => {
+		console.log(playerList[socket.id])
+		let roomNumber = playerList[socket.id]["room_number"]
+		let player = playerList[socket.id]["player"]
+		let chosen = payload["chosen"]
+		// console.log(playerList[socket.id])
+		let nickname = playerList[socket.id]["nickname"]
+		if (roomList[roomNumber][nickname]["state"] == null) {
+			roomList[roomNumber]["counter"] += 1
+		}
+
+		roomList[roomNumber][nickname]["state"] = chosen
+		console.log(roomList[roomNumber])
+		if (roomList[roomNumber]["counter"] == 2) {
+			let result = {
+				paper: {
+					stone: 1,
+					scissors: -1,
+					paper: 0,
+				},
+				stone: {
+					stone: 0,
+					scissors: 1,
+					paper: -1,
+				},
+				scissors: {
+					stone: -1,
+					paper: 1,
+					scissors: 0,
+				},
+			}
+			let homeChosen = roomList[roomNumber]["home"]["state"]
+			let visitorChosen = roomList[roomNumber]["visitor"]["state"]
+
+			let res1 = result[homeChosen][visitorChosen]
+			let res2 = {
+				1: "home",
+				"-1": "visitor",
+				0: "Draw",
+			}
+			let res3 = res2[res1]
+			roomList[roomNumber]["winner"] = res3
+			if (res3 != "Draw") {
+				roomList[roomNumber][res3]["point"] += 1
+			}
+			roomList[roomNumber]["counter"] = 0
+			io.to(roomNumber).emit(PLAYER_PLAY, roomList[roomNumber])
+			console.log(roomList[roomNumber])
+
+			setTimeout(() => {
+				roomList[roomNumber]["visitor"]["state"] = null
+				roomList[roomNumber]["home"]["state"] = null
+				roomList[roomNumber]["winner"] = null
+				io.to(roomNumber).emit(PLAYER_PLAY, roomList[roomNumber])
+			}, 2500)
+			return
+		}
+
+		io.to(roomNumber).emit(PLAYER_PLAY, roomList[roomNumber])
+	})
+})
+
+server.listen(3000, () => {
+	console.log("listening on *:3000")
+})
